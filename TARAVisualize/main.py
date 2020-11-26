3f27ea7f768da100058569399821a4486b58ad03
Christopher Neely <christopher.neely1200@gmail.com>
2020-11-25 17:36:58 -0800

    Add initial code for taxonomy viewing comparisons

--- TARAVisualize/main.py
+++ TARAVisualize/main.py
@@ -1,23 +1,37 @@
+import os
 import altair as alt
 import streamlit as st
 import pandas as pd
 
-tax_levels = ("kingdom", "clade", "phylum", "class", "subclass", "order", "family", "genus", "species")
+"""
+Script to visualize TARA oceans data
+
+"""
+
+# Path to taxonomy data
+DATA_FILE = os.path.join(os.path.dirname(__file__), "data/tax-summary-adj.tsv")
+# Various constants for visualization
+TAX_LEVELS = ("kingdom", "clade", "phylum", "class", "subclass", "order", "family", "genus", "species")
+TITLE = "TARA oceans data visualizer"
+FILTER_REGIONS = ("region", "depth")
 
 
 @st.cache
 def load_data():
-    return pd.read_csv("/home/kmarvos/Data/Analysis/data/tax-summary-adj.tsv", delimiter="\t", na_values=".",
-                       index_col=0, dtype="object")
+    return pd.read_csv(DATA_FILE, delimiter="\t", na_values=".", index_col=0, dtype="object")
 
 
-st.title("TARA oceans data visualizer")
-
+# Create simple layout
+st.title(TITLE)
+st.sidebar.write(TITLE)
+# Get view selection and display for user
+filter_selection = st.sidebar.selectbox("Filter", FILTER_REGIONS)
+st.write("Viewing by %s" % filter_selection)
+# Load cached file data
 data_raw = load_data()
 
-filter_selection = st.sidebar.selectbox("Filter data by:", ("region", "depth"))
-
-for col in tax_levels:
+# Generate each taxonomy plot
+for col in TAX_LEVELS:
     subset = data_raw[[filter_selection, "size_fraction", col]]
     c = alt.Chart(subset.dropna()).mark_bar(
         cornerRadiusTopLeft=3,
