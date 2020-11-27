f0638b8d6136f72f3ff2f335ddd40359f0806f52
Christopher Neely <christopher.neely1200@gmail.com>
2020-11-27 14:02:56 -0800

    Add FastANI visualization for selected regions

--- TARAVisualize/main.py
+++ TARAVisualize/main.py
@@ -1,13 +1,16 @@
 import os
+import numpy as np
 import pandas as pd
 import altair as alt
+import seaborn as sns
 import streamlit as st
-from typing import Dict
+from typing import Dict, List, Tuple
+import matplotlib.pyplot as plt
 
 FILE_DIR = os.path.dirname(__file__)
 DATA_FILE = os.path.join(FILE_DIR, "data/tax-summary.tsv")
 FASTANI_A = os.path.join(FILE_DIR, "data/all-alex-v-alex.fastani.out")
-FASTANI_B = os.path.join(FILE_DIR, "data/all-del-v-alex.fastani.out")
+FASTANI_B = os.path.join(FILE_DIR, "data/all-alex-v-delmont.fastani.out")
 TAX_LEVELS = ("kingdom", "clade", "phylum", "class", "subclass", "order", "family", "genus", "species")
 TITLE = "TARA oceans data visualizer"
 FILTER_BY_OPTIONS = ("size_fraction", "depth")
@@ -19,12 +22,24 @@ def load_taxonomy() -> pd.DataFrame:
 
 
 @st.cache
-def load_fastani(file_name: str) -> pd.DataFrame:
-    pass
+def load_fastani(file_name: str, header_ids: List[str]) -> pd.DataFrame:
+    return pd.read_csv(file_name, delimiter="\t", header=0, names=header_ids, dtype="object")
+
+
+def create_heatmap(file_names: List[str], as_dict: Dict[Tuple[str, str], str]) -> pd.DataFrame:
+    out = np.full((len(file_names), len(file_names)), 70.0, dtype="float32")
+    for i, f1_name in enumerate(file_names):
+        for j, f2_name in enumerate(file_names):
+            if (f1_name, f2_name) in as_dict.keys():
+                out[i, j] = float(as_dict[(f1_name, f2_name)])
+    return pd.DataFrame(out, index=file_names, columns=file_names)
 
 
 # Load cached file data
 data_raw = load_taxonomy()
+fastani_a = load_fastani(FASTANI_A, ["alexander1", "alexander2", "pid", "r1", "r2"])
+fastani_a_dict = {(k1, k2): p for k1, k2, p in zip(fastani_a.alexander1, fastani_a.alexander2, fastani_a.pid)}
+fastani_b = load_fastani(FASTANI_B, ["alexander1", "delmont", "pid", "r1", "r2"])
 # Create simple layout
 st.title(TITLE)
 st.sidebar.write(TITLE)
@@ -46,7 +61,8 @@ for col in tax_selection:
         continue
     st.write("Level: %s" % col)
     st.write(subset)
-    c = alt.Chart(subset[[col, "region", filter_selection]].dropna()).mark_bar(
+    subset = subset[[col, "region", filter_selection]].dropna()
+    c = alt.Chart(subset).mark_bar(
         cornerRadiusTopLeft=3,
         cornerRadiusTopRight=3,
         width=12
@@ -57,3 +73,6 @@ for col in tax_selection:
         column="region"
     ).configure_axisX(labelAngle=-45)
     st.altair_chart(c)
+    ax = sns.heatmap(create_heatmap(subset.index, fastani_a_dict), square=True,  cmap="mako")
+    st.pyplot(plt)
+    plt.clf()
