a6510411e1b75b2f8cb958941d776aac592fa864
Christopher Neely <christopher.neely1200@gmail.com>
2021-02-08 10:25:24 -0800

    Add initial taxonomy display

--- TARAVisualize/loader/taxonomy.py
+++ TARAVisualize/loader/taxonomy.py
@@ -7,11 +7,6 @@ def get_taxonomy(metadata: pd.DataFrame):
     levels = ["clade", "kingdom", "phylum", "class", "order", "family", "genus", "species"]
     st.title("Taxonomy")
     col1, col2 = st.beta_columns([1, 1])
-    i = 0
-    for _ in range(4):
-        data = metadata[levels[i]].dropna()
-        col1.plotly_chart(px.pie(data, names=levels[i], title=levels[i]))
-        i += 1
-        data = metadata[levels[i]].dropna()
-        col2.plotly_chart(px.pie(data, names=levels[i], title=levels[i]))
-        i += 1
+    for i in range(0, 8, 2):
+        col1.plotly_chart(px.pie(metadata[levels[i]].dropna(), names=levels[i], title=levels[i]))
+        col2.plotly_chart(px.pie(metadata[levels[i + 1]].dropna(), names=levels[i + 1], title=levels[i + 1]))
