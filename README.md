60cddb5431deebc0fe9c469411a91d87b2e1f190
Christopher Neely <christopher.neely1200@gmail.com>
2021-02-08 10:37:24 -0800

    Update install instructions

--- README.md
+++ README.md
@@ -10,13 +10,11 @@ Dependencies: `conda`, `git`
 git clone https://github.com/cjneely10/TARA-Analysis.git
 cd TARA-Analysis
 ./install.sh
-conda activate TARA-Analysis
-cd component-template/template/my_component/frontend
-npm install react-plotly.js plotly.js @types/react-plotly.js
-npm install
 ```
 
 ### Usage
+
+Running the visualizer will use &ge; 450MB of system RAM.
 
 Activate your conda environment (if not done so already):
 
--- install.sh
+++ install.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/env bash
 
+# Create conda environment
 conda env create -f environment.yml
 
+# Identify install source
+CONDA=`which conda`
+CONDA_DIRNAME=`dirname $CONDA`
+MINICONDA=`dirname $CONDA_DIRNAME`
+SOURCE=$MINICONDA/etc/profile.d/conda.sh
+
+# Activate new environment
+source $SOURCE
+conda activate TARA-Analysis
+
+# Pull streamlit-components templates
 cd TARAVisualize/components
 git clone https://github.com/streamlit/component-template.git
 cp *.tsx component-template/template/my_component/frontend/src/
 rm component-template/template/my_component/frontend/__init__.py
+
+# Install dependencies and custom components
+cd component-template/template/my_component/frontend
+npm install react-plotly.js plotly.js @types/react-plotly.js
+npm install
