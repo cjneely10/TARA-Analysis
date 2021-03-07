aa0f561b37319300780cca996b067bd1e0e95980
Christopher Neely <christopher.neely1200@gmail.com>
2021-03-06 22:33:57 -0800

    Speed up filter to be single pass

--- TARAVisualize/loader/distribution.py
+++ TARAVisualize/loader/distribution.py
@@ -5,8 +5,7 @@ from TARAVisualize import st
 
 def distribution(metadata: pd.DataFrame):
     st.title("Distribution")
-    col1, col2 = st.beta_columns([5, 5])
-    col1.altair_chart(alt.Chart(metadata).mark_bar(
+    st.altair_chart(alt.Chart(metadata).mark_bar(
         cornerRadiusTopLeft=3,
         cornerRadiusTopRight=3,
         width=12
@@ -15,7 +14,7 @@ def distribution(metadata: pd.DataFrame):
         y=alt.Y('count():O'),
         column="region"
     ).configure_axisX(labelAngle=-45))
-    col2.altair_chart(alt.Chart(metadata).mark_bar(
+    st.altair_chart(alt.Chart(metadata).mark_bar(
         cornerRadiusTopLeft=3,
         cornerRadiusTopRight=3,
         width=12
