e7dd3338dc03d3ba51c5bbe6c432e1489a53546d
Christopher Neely <christopher.neely1200@gmail.com>
2021-03-22 21:24:38 -0700

    Fix QC and ANI labels; Add ability to cluster ANI results

--- TARAVisualize/loader/fastani.py
+++ TARAVisualize/loader/fastani.py
@@ -1,14 +1,33 @@
 from typing import Set
 
+from scipy.cluster.hierarchy import linkage, dendrogram
+
 from TARAVisualize import pd
 from TARAVisualize import px
 from TARAVisualize import st
 
 
+def hClust_euclidean(genome_df):
+    linkage_matrix = linkage(genome_df, method='average', metric='euclidean')
+    # linkage_matrix = linkage(df, metric='braycurtis')
+    names = genome_df.index.tolist()
+    # clust = dendrogram(linkage_matrix, orientation="right", labels=names, get_leaves=True)
+    clust = dendrogram(linkage_matrix, no_plot=True, labels=names, get_leaves=True)
+    leaves = clust['ivl']
+    leave_order = list(leaves)
+    genome_df = genome_df.reindex(leave_order)
+
+    return genome_df
+
+
 def generate_fastani(heatmap_df: pd.DataFrame):
     # Generate ANI plot and table
     st.title("Average Nucleotide Identity")
-    st.plotly_chart(px.imshow(heatmap_df.to_numpy(), x=heatmap_df.index, y=heatmap_df.columns), clear_figure=True)
+    cluster = st.checkbox("Cluster?")
+    if cluster:
+        heatmap_df = hClust_euclidean(heatmap_df)
+    st.plotly_chart(px.imshow(heatmap_df.to_numpy(), labels={"color": "ANI"},
+                              x=heatmap_df.index, y=heatmap_df.columns), clear_figure=True)
 
 
 def filter_fastani(fastani_df: pd.DataFrame, subset_ids: Set[str]) -> pd.DataFrame:
--- TARAVisualize/loader/quality.py
+++ TARAVisualize/loader/quality.py
@@ -4,7 +4,8 @@ from TARAVisualize import st
 
 
 def generate_quality(quality: pd.DataFrame):
+    quality = quality.reset_index()
     st.title("Quality metrics")
     col1, col2 = st.beta_columns([2, 5])
-    col1.plotly_chart(px.box(quality["n50"], points="all", width=250))
-    col2.plotly_chart(px.box(quality.drop("n50", axis=1), points="all"))
+    col1.plotly_chart(px.box(quality[["n50", "ID"]], points="all", width=250, hover_name="ID"))
+    col2.plotly_chart(px.box(quality.drop("n50", axis=1), points="all", hover_name="ID"))
