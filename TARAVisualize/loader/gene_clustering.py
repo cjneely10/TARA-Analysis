66d388dae60744262bf1c00a2a9d4703c1a770f5
Christopher Neely <christopher.neely1200@gmail.com>
2021-03-23 10:42:51 -0700

    Add simple KEGG clustering - need to add more here

--- TARAVisualize/loader/gene_clustering.py
+++ TARAVisualize/loader/gene_clustering.py
@@ -1,11 +1,16 @@
 from collections import Counter
 
+from sklearn import preprocessing
+
 from TARAVisualize import pd
+from TARAVisualize import px
 from TARAVisualize import st
+from sklearn.decomposition import PCA
 
 
 def generate_kegg_plot(kegg_df: pd.DataFrame, kegg_ids):
     st.title("Top KEGG terms")
+    generate_kegg_pca(kegg_df)
     counted = Counter(kegg_df.astype(bool).sum(axis=0).to_dict())
     out = {}
     for value, count in counted.most_common():
@@ -13,3 +18,15 @@ def generate_kegg_plot(kegg_df: pd.DataFrame, kegg_ids):
             break
         out[value] = [count, kegg_ids[value]]
     st.dataframe(pd.DataFrame.from_dict(out, orient="index", columns=["Count", "Description"]))
+
+
+def generate_kegg_pca(kegg_df: pd.DataFrame):
+    kegg = PCA(n_components=2).fit_transform(scale(kegg_df.loc[:, (kegg_df != 0).any(axis=0)]))
+    st.plotly_chart(px.scatter(kegg[:, 0], kegg[:, 1]), clear_figure=True)
+
+
+def scale(df):
+    x = df.values  # returns a numpy array
+    min_max_scaler = preprocessing.MinMaxScaler()
+    x_scaled = min_max_scaler.fit_transform(x)
+    return pd.DataFrame(x_scaled, columns=df.columns, index=df.index)
