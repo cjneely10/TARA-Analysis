1d97c3ea990a19315b166d30d5d7876a001e19e5
Christopher Neely <christopher.neely1200@gmail.com>
2021-07-21 23:34:10 -0700

    Add build

---
+++ TARAVisualize/components/component-template/template/my_component/frontend/build/precache-manifest.81ba9c30ced459e13dfc06cf212eb1df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e78d6b2a0c5e1c1cd3220c8408c9205",
+    "url": "./index.html"
+  },
+  {
+    "revision": "51a05d0f91e679209637",
+    "url": "./static/js/2.2294f30f.chunk.js"
+  },
+  {
+    "revision": "3833e4d9499170e3c4466078f24614f0",
+    "url": "./static/js/2.2294f30f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9791bf8b3a10a88b93d2",
+    "url": "./static/js/main.f540ee93.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
--- TARAVisualize/components/selectable_component.py
+++ TARAVisualize/components/selectable_component.py
@@ -1,9 +1,10 @@
+import os
+
 import streamlit.components.v1 as components
 
-_component_func = components.declare_component(
-    "my_component",
-    url="http://localhost:3001",
-)
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+build_dir = os.path.join(parent_dir, "component-template/template/my_component/frontend/build")
+_component_func = components.declare_component("my_component", path=build_dir)
 
 
 def selectable_component(fig):
