85cfdcab0c8ecad03ac15547213741c775cb52e0
Christopher Neely <christopher.neely1200@gmail.com>
2021-07-22 00:02:31 -0700

    Load

--- TARAVisualize/components/selectable_component.py
+++ TARAVisualize/components/selectable_component.py
@@ -4,7 +4,7 @@ import streamlit.components.v1 as components
 
 parent_dir = os.path.dirname(os.path.abspath(__file__))
 build_dir = os.path.join(parent_dir, "component-template/template/my_component/frontend/build")
-_component_func = components.declare_component("my_component", path="TARA/Visualize/components/component-template/template/my_component/frontend/build")
+_component_func = components.declare_component("my_component", path="TARAVisualize/components/component-template/template/my_component/frontend/build")
 
 
 def selectable_component(fig):
