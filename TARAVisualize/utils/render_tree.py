1e76c9fca4d6dd21bcccf7dc0622273e8c56306f
Christopher Neely <christopher.neely1200@gmail.com>
2021-07-21 20:48:15 -0700

    Debugging

--- TARAVisualize/utils/render_tree.py
+++ TARAVisualize/utils/render_tree.py
@@ -1,11 +1,12 @@
 import sys
 
-import ete3
+from ete3.treeview.main import TreeStyle
+from ete3.coretype.tree import Tree
 
 assert len(sys.argv) == 3
 
 # Render as half-circle
-circular_style = ete3.TreeStyle()
+circular_style = TreeStyle()
 circular_style.mode = "c"
 circular_style.scale = 20
 circular_style.show_branch_support = True
@@ -13,6 +14,6 @@ circular_style.show_branch_support = True
 circular_style.arc_start = 180
 circular_style.arc_span = 270
 # Generate tree
-tree = ete3.Tree(sys.argv[1])
+tree = Tree(sys.argv[1])
 # Render as circular plot
 tree.render(sys.argv[2], tree_style=circular_style, w=800, units="px")
