68fe67842073f19a0e722c09c06064112232bd3b
Christopher Neely <christopher.neely1200@gmail.com>
2021-07-21 23:45:24 -0700

    Update download implementation; Keep attempting load

--- TARAVisualize/loader/download_selection.py
+++ TARAVisualize/loader/download_selection.py
@@ -1,7 +1,20 @@
+import base64
 import os
 from typing import List
 from TARAVisualize import pd
 from TARAVisualize import st
+
+
+# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806/2
+def get_table_download_link(df):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
+    return href
 
 
 def download_selected_mag_data(dataframes: List[pd.DataFrame]):
@@ -11,8 +24,10 @@ def download_selected_mag_data(dataframes: List[pd.DataFrame]):
     """
     if st.sidebar.button("Download selection data"):
         out = pd.DataFrame()
-        out_file = os.path.join(os.getcwd(), 'out.tsv')
+        st.sidebar.text(f"Collecting data")
         for df in dataframes:
             out = out.join(df, how="outer")
-        out.to_csv(out_file, sep="\t")
-        st.sidebar.text(f"Saved to {out_file}")
+        st.markdown(get_table_download_link(out), unsafe_allow_html=True)
+        # out.to_csv(out_file, sep="\t")
+        # st.sidebar.text(f"Saved to {out_file}")
+        st.sidebar.text("Saved!")
