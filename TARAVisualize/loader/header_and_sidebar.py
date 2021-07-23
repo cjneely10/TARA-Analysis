7ac0a3c5c8cac8bbe73beb383520295f4481789d
Christopher Neely <christopher.neely1200@gmail.com>
2021-07-22 17:20:43 -0700

    Move around taxonomy

--- TARAVisualize/loader/header_and_sidebar.py
+++ TARAVisualize/loader/header_and_sidebar.py
@@ -11,11 +11,24 @@ def get_mags_list(metadata: pd.DataFrame) -> Set[str]:
     st.sidebar.write(title)
     st.sidebar.write("Filter by")
     # Allow user to select which subregions to compare
+    regions_selection = st.sidebar.multiselect("Regions", sorted(list(set(metadata.region))))
+    size_fraction_selection = st.sidebar.multiselect("Size Fraction",
+                                                     sorted(list(set(metadata.size_fraction)),
+                                                            key=lambda val: int(val.split("-")[0])))
+    depth_selection = st.sidebar.multiselect("Depth", sorted(list(set(metadata.depth))))
     taxonomy_selection = st.sidebar.selectbox("Taxonomic Level", ["all", *tax_levels])
     filtered_data = metadata
 
     to_filter = []
     filter_used = False
+    if len(regions_selection) + len(size_fraction_selection) + len(depth_selection) > 0:
+        # Display filtered results as table
+        if len(regions_selection) > 0:
+            to_filter.append(("region", "regions_selection"))
+        if len(size_fraction_selection) > 0:
+            to_filter.append(("size_fraction", "size_fraction_selection"))
+        if len(depth_selection) > 0:
+            to_filter.append(("depth", "depth_selection"))
 
     if len(to_filter) > 0:
         filtered_data = filtered_data[eval("&".join([('filtered_data["%s"].isin(%s)' % (label, selection))
@@ -28,19 +41,6 @@ def get_mags_list(metadata: pd.DataFrame) -> Set[str]:
         filtered_data = filtered_data[filtered_data[taxonomy_selection].isin(levels_selection)]
         filter_used = True
         # Show dataframe on menu for selection
-    regions_selection = st.sidebar.multiselect("Regions", sorted(list(set(metadata.region))))
-    size_fraction_selection = st.sidebar.multiselect("Size Fraction",
-                                                     sorted(list(set(metadata.size_fraction)),
-                                                            key=lambda val: int(val.split("-")[0])))
-    depth_selection = st.sidebar.multiselect("Depth", sorted(list(set(metadata.depth))))
-    if len(regions_selection) + len(size_fraction_selection) + len(depth_selection) > 0:
-        # Display filtered results as table
-        if len(regions_selection) > 0:
-            to_filter.append(("region", "regions_selection"))
-        if len(size_fraction_selection) > 0:
-            to_filter.append(("size_fraction", "size_fraction_selection"))
-        if len(depth_selection) > 0:
-            to_filter.append(("depth", "depth_selection"))
     st.sidebar.write(f"{len(filtered_data)} MAGs selected")
     if filter_used:
         return set(filtered_data.index)
