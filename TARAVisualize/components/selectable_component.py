365c907370ec824d7b876ba4cf43a7d1f7c61366
Christopher Neely <christopher.neely1200@gmail.com>
2022-01-12 12:14:19 -0800

    Proper pathing

--- TARAVisualize/components/selectable_component.py
+++ TARAVisualize/components/selectable_component.py
@@ -1,9 +1,8 @@
 import os
 import streamlit.components.v1 as components
 
-build = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                     "component-template/template/my_component/frontend/build")
-_component_func = components.declare_component("my_component", path=build)
+_component_func = components.declare_component("my_component", path="TARAVisualize/components/component-template"
+                                                                    "/template/my_component/frontend/build")
 
 
 def selectable_component(fig):
