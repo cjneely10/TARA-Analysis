d47eb009e2d7808ed10997536700c4d664d6953a
Christopher Neely <christopher.neely1200@gmail.com>
2023-08-11 11:13:27 -0700

    Adjust write directory

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,7 @@ WORKDIR /app
 COPY . .
 RUN apt-get update && \
     apt-get install $(tr '\n' ' ' < packages.txt) -y && \
-    pip install -r requirements.txt \
-RUN chown -R adminuser:adminuser /home/adminuser/venv/lib/python3.11/site-packages/TARAVisualize/utils
+    pip install -r requirements.txt
 ENV PATH /opt/conda/envs/TARA-Analysis/bin:$PATH
-USER adminuser
 CMD ["streamlit", "run", "TARAVisualize/main.py"]
 EXPOSE 8501
--- TARAVisualize/utils/tree_subsetter.py
+++ TARAVisualize/utils/tree_subsetter.py
@@ -15,8 +15,8 @@ class TreeSubsetter:
         self.tree = ete3.Tree(tree_path.read_text())
         self.id =  ''.join(random.choices(string.ascii_uppercase +
                              string.digits, k=20))
-        self.tmp_tree = os.path.join(os.path.dirname(__file__), f"{self.id}.newick")
-        self.tmp_png = os.path.join(os.path.dirname(__file__), f"{self.id}.png")
+        self.tmp_tree = os.path.join("~", f"{self.id}.newick")
+        self.tmp_png = os.path.join("~", f"{self.id}.png")
 
     def prune(self, ids: Set[str]) -> str:
         self.clean()
